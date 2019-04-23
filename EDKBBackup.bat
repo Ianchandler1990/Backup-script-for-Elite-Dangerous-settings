e8bafc3424f6125fbe2526e92e426d249da92524
IanChandler <cell600@hotmail.com>
2019-04-23 20:37:27 +0100

    Added Statments to Let you pick yes or no to each folder

--- EDKBBackup.bat
+++ EDKBBackup.bat
@@ -3,17 +3,31 @@
 set drive=E:\EDKBBackup
 set backupcmd=xcopy /s /c /d /e /h /i /r /y
 
-echo ### Backing Up ED Key Binds
+SET /P Bindsyesno=Do you wish to proceed to backup your Key Binds? [y/n]:
+IF "%Bindsyesno%"=="y" GOTO KeyBinds
+IF "%Bindsyesno%"=="Y" GOTO KeyBinds
+GOTO Journal
 
+:KeyBinds
+echo ### Backing Up ED Key Binds
 %backupcmd% "C:\Users\%username%\AppData\Local\Frontier Developments\Elite Dangerous\Options\Bindings" %drive%\KeyBinds
 
-@pause
+:Journal
+SET /P Journalyesno=Do you wish to proceed to backup your Journal? [y/n]:
+IF "%Journalyesno%"=="y" GOTO Journal
+IF "%Journalyesno%"=="Y" GOTO Journal
+GOTO Graphics
 
 echo ### Backing Up ED Journal
 %backupcmd% "C:\Users\%username%\Saved Games\Frontier Developments\Elite Dangerous" %drive%\Journal
 
-@pause
+:Graphics
+SET /P Graphicsyesno=Do you wish to proceed to backup your Graphics? [y/n]:
+IF "%Graphicsyesno%"=="y" GOTO Graphics
+IF "%Graphicsyesno%"=="Y" GOTO Graphics
+GOTO End
 
+:Graphics
 echo ### Backing Up ED Graphics
 %backupcmd% "C:\Users\%username%\AppData\Local\Frontier Developments\Elite Dangerous\Options\Graphics" %drive%\Graphics
 
@@ -21,4 +35,5 @@ echo ### Backing Up ED Graphics
 :: %backupcmd% "...source directory..." "%drive%\...destination dir..."
 
 echo Backup Complete!
-@pause
+
+:End
