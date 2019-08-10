ac2e8355348ce0e839f684c508f3bed892554520
IanChandler <cell600@hotmail.com>
2019-08-10 07:48:40 +0100

    Update EDKBBackup.bat
    Added Restore option

--- EDKBBackup.bat
+++ EDKBBackup.bat
@@ -2,10 +2,19 @@
 :: variables Drive= Place to backup files
 set drive=E:\EDKBBackup
 set backupcmd=xcopy /s /c /d /e /h /i /r /y
+set restorecmd=xcopy /s /c /e /h /i /r /y
 
+:Process
+SET /P Process=Do you want to backup or restore? [backup (b)/restore (r)]:
+IF "%Process%"=="r" GOTO restore
+IF "%Process%"=="b" GOTO Backup
+IF "%Process%"=="" GOTO Process
+
+:Backup
 SET /P Bindsyesno=Do you wish to proceed to backup your Key Binds? [y/n]:
 IF "%Bindsyesno%"=="y" GOTO KeyBinds
 IF "%Bindsyesno%"=="n" GOTO Journalstart
+IF "%Bindsyesno%"=="" GOTO Backup
 
 :KeyBinds
 echo ### Backing Up ED Key Binds
@@ -15,6 +24,7 @@ echo ### Backing Up ED Key Binds
 SET /P Journalyesno=Do you wish to proceed to backup your Journal? [y/n]:
 IF "%Journalyesno%"=="y" GOTO Journal
 IF "%Journalyesno%"=="n" GOTO Graphicsstart
+IF "%Journalyesno%"=="" GOTO KeyBinds
 
 :Journal
 echo ### Backing Up ED Journal
@@ -24,6 +34,7 @@ echo ### Backing Up ED Journal
 SET /P Graphicsyesno=Do you wish to proceed to backup your Graphics? [y/n]:
 IF "%Graphicsyesno%"=="y" GOTO Graphics
 IF "%Graphicsyesno%"=="n" GOTO End
+IF "%Graphicsyesno%"=="" GOTO Graphicsstart
 
 :Graphics
 echo ### Backing Up ED Graphics
@@ -34,4 +45,14 @@ echo ### Backing Up ED Graphics
 
 echo Backup Complete!
 
+GOTO End
+
+:restore
+echo ### Restoring ED Key Binds
+%restorecmd% "E:\EDKBBackup\KeyBinds" "C:\Users\%username%\AppData\Local\Frontier Developments\Elite Dangerous\Options\Bindings"
+%restorecmd% "E:\EDKBBackup\Graphics" "C:\Users\%username%\AppData\Local\Frontier Developments\Elite Dangerous\Options\Graphics"
+%restorecmd% "E:\EDKBBackup\Journal" "C:\Users\%username%\Saved Games\Frontier Developments\Elite Dangerous"
+echo ### restore done
+
 :End
+timeout 5
